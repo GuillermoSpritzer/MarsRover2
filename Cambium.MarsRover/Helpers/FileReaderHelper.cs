2ee701743d5a4c9f7db390fb288a026f6d7b15a7
GuillermoSpritzer <guispritzer@gmail.com>
2021-03-10 09:19:42 +0000

    test

--- Cambium.MarsRover/Helpers/FileReaderHelper.cs
+++ Cambium.MarsRover/Helpers/FileReaderHelper.cs
@@ -33,7 +33,7 @@ namespace Cambium.MarsRover.Web
                     }
                 }
             }
-            catch (Exception e)
+            catch (Exception)
             {
                 throw new InvalidFileException("File is invalid");
             }
